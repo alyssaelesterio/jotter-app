6b747605fb6955ec87de6e2d0b35cfcda768855b
alyssaelesterio <aklelesterio@gmail.com>
2021-12-05 23:10:45 +0800

    home: sticky-nav is done and added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainSectionComponent } from './main-section/main-section.component';
 
@@ -8,10 +8,31 @@ import { MainSectionComponent } from './main-section/main-section.component';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  sticky: boolean = false;
+  menuPosition: any;
+  elementPosition: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild('stickyMenu') menuElement: ElementRef;
+
+
+  ngAfterViewInit(){
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+
+
+  @HostListener('window:scroll', ['$event'])
+    handleScroll(){
+      const windowScroll = window.pageYOffset;
+      console.log(windowScroll, this.elementPosition);
+      if(windowScroll >= 97){
+        this.sticky = true;
+      } else {
+        this.sticky = false;
+      }
+    }
 }
