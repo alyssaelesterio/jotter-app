8e319756c0d349136f2b78fa38860d15a0fb0bac
aazucena <aazucena@gotabrand.com>
2021-12-05 15:46:02 +0800

    main-section bg-image is set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { MainSectionComponent } from './home/main-section/main-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    MainSectionComponent
   ],
   imports: [
     BrowserModule,
