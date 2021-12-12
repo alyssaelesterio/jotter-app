bc093862ee96daabee82b1785c6ab5f11df3b03f
alyssaelesterio <aklelesterio@gmail.com>
2021-12-12 19:19:24 +0800

    home: features design import done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './home/navbar/navbar.component';
 import { MainSectionComponent } from './home/main-section/main-section.component';
+import { FeaturesComponent } from './home/features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    MainSectionComponent
+    MainSectionComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MainSectionComponent } from './main-section/main-section.component';
 
 @Component({
   selector: 'app-home',
