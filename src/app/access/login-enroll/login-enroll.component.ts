8caaab356313daf891230b1514ff0117e0f8f65d
alyssa elesterio <aklelesterio@gmail.com>
2021-12-28 13:29:02 +0800

    login-enroll: animations udpated u1

--- src/app/access/login-enroll/login-enroll.component.ts
+++ src/app/access/login-enroll/login-enroll.component.ts
@@ -6,8 +6,6 @@ import {
   style,
   transition,
   animate,
-  keyframes,
-  group
 } from '@angular/animations';
 
 @Component({
@@ -16,35 +14,46 @@ import {
   styleUrls: ['./login-enroll.component.scss'],
   animations: [
     trigger('boxTwoState', [
-      state('left', style({
-        
-      })),
       state('right', style({
+        opacity: 1,
         transform : 'translateX(-470px)',
         'border-radius' : '20px 0 0 20px',
         'z-index' : '10'
       })),
-      transition('left <=> right', animate(1000))
+      transition('left <=> right', animate(500)),
     ]),
 
     trigger('boxOneState', [
       state('left', style({
         transform : 'translateX(300px)',
         'border-radius' : '0px 20px 20px 0px',
-        'z-index' : '5'
+        'z-index' : '10'
       })),
-      state('right', style({
-        
-      })),
-      transition('left <=> right', animate(1000))
+      transition('left <=> right', animate(500))
     ]),
-  ]
+
+    trigger('containerState', [
+      state('false', style({
+        opacity: 1,
+        transform: 'translateY(10px)'
+      })),
+      transition('void <=> *', [
+        style({
+          opacity: 0,
+          transform: 'translateY(-10px)'
+        }),
+        animate(1000)
+      ])
+    ]),
+
+  ] //end of animation
 })
 export class LoginEnrollComponent implements OnInit {
   boxOneState = "right";
   boxTwoState = "left";
   loginState = true;
   enrollState = false;
+  containerState = false;
 
 
   constructor() { }
@@ -57,7 +66,6 @@ export class LoginEnrollComponent implements OnInit {
     this.boxTwoState == 'left' ? this.boxTwoState = 'right' : this.boxTwoState = "left";
     this.boxOneState == 'left' ? this.boxOneState = 'right' : this.boxOneState = "left";
     this.loginState == true ? this.loginState = false : this.loginState = true;
-  
   }
 
 }
