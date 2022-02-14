11cdaea4ffc2cff37f619e2f361ad9f66696f165
alyssaelesterio <aklelesterio@gmail.com>
2022-02-14 12:53:14 +0800

    main: notebooks and notes list from flex to grid

--- src/app/main/notebooks/notebooks.component.ts
+++ src/app/main/notebooks/notebooks.component.ts
@@ -13,4 +13,15 @@ export class NotebooksComponent implements OnInit {
     this.sideNavOpened=true
   }
 
+ 
+
+
+  // const grid = Array.from(document.querySelector("#grid").children);
+  // const baseOffset = grid[0].offsetTop;
+  // const breakIndex = grid.findIndex(item => item.offsetTop > baseOffset);
+  // const numPerRow = (breakIndex === -1 ? grid.length : breakIndex);
+
+  //rawr = Array.from(Document.querySelector('ul'));
+
+
 }
