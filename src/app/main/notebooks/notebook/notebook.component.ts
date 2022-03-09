0a765f688e054e0c2b9ec9c7d07c6d0c3eb71239
alyssaelesterio <aklelesterio@gmail.com>
2022-03-09 18:30:23 +0800

    main: notebook tools fx added

--- src/app/main/notebooks/notebook/notebook.component.ts
+++ src/app/main/notebooks/notebook/notebook.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notebook.component.scss']
 })
 export class NotebookComponent implements OnInit {
+  triggerFx: boolean = false;
+  
+  centered = true;
+  unbounded = true;
+
+  radius: number = 25;
+  color: string = "rgb(255, 255, 255, 0.30)";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+  onHover() {
+    //alert('hover working');
+    console.log('test');
+    this.triggerFx = this.triggerFx ? false : true;
+    console.log(this.triggerFx);
+  }
 }
